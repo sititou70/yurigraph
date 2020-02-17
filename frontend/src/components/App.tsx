9e8274d23071201bb5d714a170c0586929ae5258
Takuya Shizukuishi <sititou70@gmail.com>
2020-02-18 00:23:29 +0900

    feat: google analytics

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DereGraph from './graph/DereGraph';
 import { Global } from '@emotion/core';
 import { global_style } from '../styles/GlobalStyle';
 import Menu from './Menu';
 import CouplingRanking from './CouplingRanking';
+import ReactGA from 'react-ga';
 
 // components
-export const App = () => (
-  <div>
-    <Global styles={global_style} />
-    <DereGraph />
-    <CouplingRanking />
-    <Menu />
-  </div>
-);
+export const App = () => {
+  useEffect(() => {
+    ReactGA.initialize('UA-158683797-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+    <div>
+      <Global styles={global_style} />
+      <DereGraph />
+      <CouplingRanking />
+      <Menu />
+    </div>
+  );
+};
 
 export default App;
