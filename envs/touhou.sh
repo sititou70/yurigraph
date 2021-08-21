29948241bf34985accad40426e84604b4a851330
Takuya Shizukuishi <sititou70@gmail.com>
2021-08-21 09:07:10 +0900

    fix: touhou theme color

--- envs/touhou.sh
+++ envs/touhou.sh
@@ -6,7 +6,9 @@ export REACT_APP_NAME="TouhouMap"
 export REACT_APP_DESCRIPTION="東方Projectのカップリングを可視化"
 export REACT_APP_TITLE_TAG="$REACT_APP_NAME | $REACT_APP_DESCRIPTION"
 export REACT_APP_DEFAULT_FILTER_VALUE=350
-export REACT_APP_MAIN_COLOR="#fef263"
+# 天色 https://www.colordic.org/colorsample/2312
+export REACT_APP_MAIN_COLOR="#2ca9e1"
+# 紅 https://www.colordic.org/colorsample/2014
 export REACT_APP_ACCENT_COLOR="#d7003a"
 export DEPLOY_REPOSITORY="git@github.com:sititou70/TouhouMap.git"
 export DEPLOY_BRANCH="gh-pages"
