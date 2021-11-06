db9c893ced8f5c87fa6b5473a19121c5912fc0fe
kobayashitakaki <o.share.thief@gmail.com>
2021-11-07 00:40:58 +0900

    feat: add target_couplings: 東京卍リベンジャーズ

---
+++ envs/revengers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+export TARGET_COUPLINGS_JSON="scraping/target_couplings/revengers/index.json"
+export REACT_APP_NAME="RevengersGraph"
+export REACT_APP_DESCRIPTION="東京卍リベンジャーズのカップリングを可視化"
+export REACT_APP_TITLE_TAG="$REACT_APP_NAME | $REACT_APP_DESCRIPTION"
+export REACT_APP_DEFAULT_FILTER_VALUE=100
+export REACT_APP_MAIN_COLOR="#5fc7d4"
+export REACT_APP_ACCENT_COLOR="#e50150"
+export DEPLOY_REPOSITORY="git@github.com:sititou70/revengersmgraph.git"
+export DEPLOY_BRANCH="gh-pages"
